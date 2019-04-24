377e5bab6c05cee9ae7df96d74363291d185481f
Brian Salemi <briandsalemi@gmail.com>
2019-04-24 12:37:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 def line(array)
   if array.length > 0
-
+      puts "The line is currently 1. #{array[0]}, 2. #{array[1]}, 3. #{array[2]}"
   else
     puts "The line is currently empty."
+  end
+end
