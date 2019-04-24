160de160cdba52c847468a5f45d88eedd7bda941
Brian Salemi <briandsalemi@gmail.com>
2019-04-24 12:53:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 # Write your code here.
-def line(array)
-  if array.length > 0
-      puts "The line is currently: 1. #{array[0]} 2. #{array[1]} 3. #{array[2]}"
+def line(katz_deli)
+  if katz_deli.length > 0
+      puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
   else
     puts "The line is currently empty."
   end
 end
 
-def take_a_number(array, name)
-  array.push(name)
-  puts "Welcome #{name}. You are number #{array.length} in line."
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving
