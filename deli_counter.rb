e7ccef759fd6a52cf03efa26f9d4bee46cb4377d
Brian Salemi <briandsalemi@gmail.com>
2019-04-24 12:51:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,3 +6,10 @@ def line(array)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome #{name}. You are number #{array.length} in line."
+end 
+
+def now_serving
